30ee88a8c52a1c74e74ea27e3a9489b6a5d229d5
ramprasathmk <mkramprasath12@gmail.com>
2025-03-20 00:14:10 +0545

    add go maps examples

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/ramprasathmk/learningGolang/src"
+	"github.com/ramprasathmk/learningGolang/src/maps"
 	"github.com/ramprasathmk/learningGolang/src/structs"
 )
 
 func main() {
 	src.Hi()
 	structs.RunStructure()
+	maps.RunMaps()
 }
