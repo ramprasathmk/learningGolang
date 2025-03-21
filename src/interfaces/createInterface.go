66e39a722f0da1fd5751b3833fb789276abd1d6f
ramprasathmk <mkramprasath12@gmail.com>
2025-03-21 22:57:45 +0545

    update interface examples

--- src/interfaces/createInterface.go
+++ src/interfaces/createInterface.go
@@ -11,23 +11,38 @@ type Shape interface {
 	CalcArea() float64
 }
 
+// create Shape Circle
 type Circle struct {
 	Radius float64
 }
 
+// calculates Shape Circle
 func (c Circle) CalcArea() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+// create Shape Triangle
 type Triangle struct {
 	Base float64
 	Height float64
 }
 
+// calculates Area of Triangle
 func (t Triangle) CalcArea() float64 {
 	return (t.Base * t.Height) / 2
 }
 
+
+// create Shape Rectange
+type Rectangle struct {
+	Length float64
+	Breadth float64
+}
+
+// calculates Area of Rectangle
+func (r Rectangle) CalcArea() float64 {
+	return r.Length * r.Breadth
+}
 
 func PrintType(i interface{}) {
 	fmt.Printf("Value: %v, Type: %T\n",i , i)
